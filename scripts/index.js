16c8538ae26eb271593507c5ab2cb8fb3733830f
Oganes Grigoryan <og.gr1goryan@yandex.ru>
2023-02-28 23:02:42 +0300

    первый коммит

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let profileEditButton = document.querySelector('.profile__editButton'),
+    profileTitle = document.querySelector('.profile__title'),
+    profileSubtitle = document.querySelector('.profile__subtitle'),
+    popupInput = document.querySelector('.popup'),
+    closeInput = popupInput.querySelector('.popup__close'),
+    nameInput = popupInput.querySelector('.popup__input_name'),
+    jobInput = popupInput.querySelector('.popup__input_job'),
+    form = popupInput.querySelector('.popup__form');
+
+function openPopap() {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  popupInput.classList.add('popup_opened');
+}
+
+function closePopap() {
+  popupInput.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopap();
+}
+
+form.addEventListener('submit', handleFormSubmit);
+profileEditButton.addEventListener('click', openPopap)
+closeInput.addEventListener('click', closePopap)
